3a6d9ba0438a90572222b9d71a94638d3d6ea2c2
Kai <72072855+Miao-Cloud@users.noreply.github.com>
2020-12-28 19:31:59 +0000

    Add files via upload

---
+++ Programming Assignment/Scripts2/Visuals.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Visuals : MonoBehaviour
+{
+    public float sightRange, attackRange;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+}
